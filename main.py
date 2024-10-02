5db9c3dbcf46a1a1086c0b164ee918edb708ca12
Mariaduda2507 <oliveirasoaresduda2@gmail.com>
2024-10-01 21:50:54 -0300

    atividade6

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
-
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -10,6 +10,20 @@ static = Jinja2Templates(directory="static")
 @app.get("/")
 def get_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/cadastro")
+def get_contato(request: Request):
+    return templates.TemplateResponse("cadastro.html", {"request": request})
+
+@app.post("/post_cadastro")
+def post_contato(
+    nome: str = Form(...),
+    descricao: str = Form(...),
+    estoque: str = Form(...),
+    preco: str = Form(...),
+    categoria: str = Form(...)):
+    salvar_cadastro(nome, descricao, estoque, preco, categoria)
+    return RedirectResponse("/", 303)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
---
+++ models/produto_model.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from datetime import date
+from numbers import Real
+from typing import Optional
+
+
+@dataclass
+class Usuario:
+    id: Optional[int] = None
+    nome: Optional[str]= None
+    descricao: Optional[str] = None
+    estoque: Optional[str] = None
+    preco: Optional[Real] = None
+    categoria: Optional[int] = None
